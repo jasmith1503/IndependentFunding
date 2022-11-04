091576df4e1e85d892310c28786faa3996faf47f
jasmith1503 <jasmith1503@gmail.com>
2022-11-03 22:17:50 -0700

    Adding project files

---
+++ crowdfunding_db_schema.sql
@@ -0,0 +1,16 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "backers" (
+    "backer_id" varchar(5)   NOT NULL,
+    "cf_id" int   NOT NULL,
+    "first_name" varchar(20)   NOT NULL,
+    "last_name" varchar(20)   NOT NULL,
+    "email" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_backers" PRIMARY KEY (
+        "backer_id"
+     )
+);
+
+SELECT * FROM backers; 
